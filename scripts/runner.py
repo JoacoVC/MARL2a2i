28b5b811d810420d40d3c91326129f0218b6f91c
Leober Duran <lduranr@unal.edu.co>
2026-06-01 17:32:27 -0500

    csvs

--- scripts/runner.py
+++ scripts/runner.py
@@ -91,7 +91,7 @@ class Runner:
 
     def run_all_experiments(self):
         ruta_base_original = self.configs['Output_csv']
-        pesos_w1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        pesos_w1 = [5, 6, 7, 8, 9, 10]
         pesos_w2 = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
         # 1. Lista para recolectar el "punto Z" de cada combinación
